e1d49152a1f0641234c4d6ad2d3e608326f09f13
main-user <bughomenoise@gmail.com>
2024-06-24 09:55:45 -0400

    init project

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "bsgender";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  # inputs.go.url = "github:nixos/nixpkgs/a343533bccc62400e8a9560423486a3b6c11a23b";
+
+  outputs = { nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            air
+          ];
+          nativeBuildInputs = [
+            # inputs.go.legacyPackages.${system}.go_1_22
+          ];
+          shellHook = ''
+            go mod download
+            go mod verify
+          '';
+        };
+
+      });
+}
